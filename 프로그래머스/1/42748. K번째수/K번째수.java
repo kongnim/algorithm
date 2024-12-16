0b90a88c40c8b881c0da2c4dd5631cfee6f05513
kongmin <amabile29@naver.com>
2024-12-16 21:31:41 +0900

    [level 1] Title: K번째수, Time: 6.95 ms, Memory: 78 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -2,13 +2,8 @@ import java.util.Arrays;
 
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
-        int[] returnArray = new int[commands.length];
-        for (int i = 0; i < commands.length; i++) {
-            int[] copiedArray = Arrays.copyOfRange(array, commands[i][0] - 1, commands[i][1]);
-            Arrays.sort(copiedArray);
-            returnArray[i] = copiedArray[commands[i][2] - 1];
-        }
-        
-        return returnArray;
+        return Arrays.stream(commands).mapToInt(command -> {
+            return Arrays.stream(array, command[0] - 1, command[1]).sorted().toArray()[command[2] - 1];
+        }).toArray();
     }
 }
